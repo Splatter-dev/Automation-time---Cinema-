d794ad3d14a1ac77501b4f79c8218e19cf34ef97
Paulo <paulovsp16@hotmail.com>
2021-01-28 17:26:04 -0300

    Finalizado a validação de opção selecionada.

--- modules/menu/__init__.py
+++ modules/menu/__init__.py
@@ -8,7 +8,7 @@ def main_menu():
     print("CINEMA - TIME OFFSET".center(60))
     print("-"*60)
     print(f"{colour.yellow('1')} - {colour.blue('Cadastrar novo filme')}")
-    print(f"{colour.yellow('2')} - {colour.blue('Listar ultimos filmes acadastrados')}")
+    print(f"{colour.yellow('2')} - {colour.blue('Listar ultimos filmes cadastrados')}")
     print(f"{colour.yellow('3')} - {colour.blue('Pesquisar por filme cadastrado')}")
     print(f"{colour.yellow('4')} - {colour.blue('Sair do sistema')}")
     print("-"*60)
--- modules/menu_validation/__init__.py
+++ modules/menu_validation/__init__.py
@@ -1,19 +1,29 @@
 import modules.colours as colour
+from modules import menu
 from modules import help_funcs
 from time import sleep
 
 
 def option_selected(option=""):
+
+    attempts = 0
+
     while True:
+        if attempts >= 2:
+            help_funcs.clear_terminal(2)
+            menu.main_menu()
+            attempts = 0
+
         try:
             option_input = int(input(colour.purple("Digite uma opção: ")))
         except(ValueError, TypeError):
             print(colour.red("Digite um valor correto!"))
-            # os.system("cls" if os.name == "nt" else 'clear')
-            help_funcs.clear_terminal(3)
-            break
+            attempts += 1
+            continue
+
         if option_input not in (1, 2, 3, 4):
             print(colour.red("Selecione uma opção valida!"))
-            help_funcs.clear_terminal(3)
-            break
+            attempts += 1 
+            continue
+
         return option_input
